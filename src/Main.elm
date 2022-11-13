c7b0d048650f15cc8174f34db1733e85abbb34ed
Wallace Nascimento <wallacediogo10@hotmail.com>
2022-11-13 20:55:20 -0300

    feature: page base

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,12 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (..)
 import Html.Attributes exposing (src)
+import Html.Attributes exposing (id)
+import Html.Attributes exposing (class)
+import Html.Attributes exposing (alt)
+import Html.Attributes exposing (href)
 
 
 ---- MODEL ----
@@ -37,8 +41,20 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ header []
+            [ img [ id "logo", src "/cbf.png", alt "logo da seleção"] []
+            , nav [ id "navLinks" ] 
+                [ a [class "links", href "#"] [ text "Noticias" ]
+                , a [class "links", href "#"] [ text "Jogadores" ]
+                , a [class "links", href "#"] [ text "Calendário" ]
+            ], a [id "button"] [ span [] [], span [] [], span [] [], span [] [], text "Loja" ] 
+            ]
+        , div [ id "mainDiv" ]
+            [ div [ id "mainText" ] 
+                [ pre [ id "titulo" ] [ text "Seleção ", span [ class "brazilColorDeg" ] [ text "Brasileira" ], text "\nVamos ser hexa!" ]
+                , pre [ id "informativo" ] [ text "2022 é ano de copa e o Brasil vai se unir\nem busca da sexta taça!\n" ]]
+            , img [ id "mainImg", src "/craques.png", alt "imagem da seleção com os jogadores Neymar, Marquinhos e Gabriel Jesus" ][]
+            ]
         ]
 
 
